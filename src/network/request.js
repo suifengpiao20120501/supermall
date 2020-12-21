8607661074d65d543f7682317bcb2bd754e01747
suifengpiao20120501 <1098264853@qq.com>
2020-12-21 16:36:14 +0800

    TabControl点击切换商品

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 export function request(config) {
   /* 1.创建axios的实例 */
   const instance = axios.create({
-    baseURL: 'http://123.207.32.32:8000',
+    // baseURL: 'http://123.207.32.32:8000',
+    baseURL: 'http://152.136.185.210:8000/api/w6',
     timeout: 5000
   })
 
@@ -16,7 +17,7 @@ export function request(config) {
     // 3.某些网络请求（比如登陆（token）），必须携带一些特殊的信息
     return config; // 拦截以后，要返回配置，不然会报错
   }, err => {
-    // console.log(err);
+    console.log(err);
   })
 
   /* 2.2.响应拦截器 */
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,7 +5,10 @@
     </nav-bar>
     <home-recommend-view :recommends="recommends"/>
     <feature-view/>
-    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"/>
+    <tab-control class="tab-control"
+                 :titles="['流行', '新款', '精选']"
+                 @tabClick="tabClick"/>
+    <goods-list :goods="showGoods"/>
     <ul>
       <li>测试</li>
       <li>测试</li>
@@ -107,8 +110,9 @@
 
   import NavBar from "components/common/navbar/NavBar";
   import TabControl from "components/content/tabControl/TabControl";
+  import GoodsList from "components/content/goods/GoodsList";
 
-  import {getHomeMultidata} from "@/network/home";
+  import { getHomeMultidata, getHomeGoods } from "@/network/home";
 
   export default {
     name: "Home",
@@ -116,20 +120,73 @@
       TabControl,
       NavBar,
       HomeRecommendView,
-      FeatureView
+      FeatureView,
+      GoodsList
     },
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        },
+        currentType: 'pop'
+      }
+    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list;
       }
     },
     created() {
       /* 1.请求多个数据 */
-      getHomeMultidata().then(res => {
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
+      this.getHomeMultidata();
+      /* 2.请求商品数据 */
+      this.getHomeGoods('pop');
+      this.getHomeGoods('new');
+      this.getHomeGoods('sell');
+    },
+    methods: {
+      /**
+       * 事件监听相关的方法
+       */
+      tabClick(index) {
+        switch (index) {
+          case 0:
+            this.currentType = 'pop';
+            break;
+          case 1:
+            this.currentType = 'new';
+            break;
+          case 2:
+            this.currentType = 'sell';
+            break;
+        }
+      },
+      /**
+       * 网络请求相关的方法
+       */
+      getHomeMultidata() {
+        /* 1.请求多个数据 */
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        // console.log(type);
+        /* 第几页 */
+        const page = this.goods[type].page + 1;
+        /* 2.请求商品数据 */
+        getHomeGoods(type, page).then(res => {
+          // console.log(res);
+          /* 使用...语法将数组中每个元素取出来后添加到数组中，页数加1 */
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+        })
+      }
     }
   }
 </script>
@@ -154,5 +211,6 @@
   .tab-control {
     position: sticky; /* 滑动页面时固定tabControl组件 */
     top: 44px; /* 距离顶部44px时，固定组件 */
+    z-index: 9;
   }
 </style>
